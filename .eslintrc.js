9a04cf9d5b8f7cbda7d17ea22afd7d215e57dd8c
denebtech <techdeneb@gmail.com>
2019-02-15 20:57:13 -0500

    new code escaping

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,6 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": ["mocha"],
-    "rules": {
-        "indent": ["warn", 4, {
-            SwitchCase: 1
-        }],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-        "space-before-function-paren": ["error", "never"],
-        "linebreak-style": ["error", "windows"],
-        "mocha/no-exclusive-tests": "error"
-    },
-    "env": {
-        "jasmine": true
-    }
+    "extends": [
+        "hive",
+        "hive/prettier"
+    ]
 };
