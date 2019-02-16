238646cd64de9547252d706a30b89a536c512d11
denebtech <techdeneb@gmail.com>
2019-02-15 20:55:25 -0500

    Add new packages

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 // requires the multiple libraries
 const express = require("express");
 const process = require("process");
+const util = require("hive-js-util");
 const lib = require("./lib");
 
 // builds the initial application object to be used
 // by the application for serving
 const app = express();
 
+process.on("SIGINT", function() {
+    process.exit();
+});
+
+process.on("SIGTERM", function() {
+    process.exit();
+});
+
 process.on("exit", () => {
-    console.log("Exiting on user's request");
+    util.Logging.info("Exiting on user's request");
     lib.destroy();
 });
 
@@ -24,6 +33,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.listen(lib.PORT, lib.HOSTNAME, () => {
-    console.log("Listening on " + lib.HOSTNAME + ":" + String(lib.PORT));
+    lib.startLogging();
+    util.Logging.info("Listening on " + lib.HOSTNAME + ":" + String(lib.PORT));
     lib.init();
 });
